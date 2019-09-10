3fa4daf516627b80b3595cd250098163e78943b7
Syd <green@Syds-MacBook-Air-2.local>
2019-09-10 10:54:13 -0500

    added boostrap

--- EmojiJabber/app/controllers/users_controller.rb
+++ EmojiJabber/app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
    
     def create
         @user = User.create(user_params)
-        # byebug
         session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
