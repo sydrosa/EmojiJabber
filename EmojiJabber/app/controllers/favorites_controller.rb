f7024ef358ff5b48f8875d463200d2680ed8d360
Cody <lawdermiltcody@gmail.com>
2019-09-10 10:30:58 -0500

    Cody's first changes

--- EmojiJabber/app/controllers/favorites_controller.rb
+++ EmojiJabber/app/controllers/favorites_controller.rb
@@ -1,2 +1,30 @@
 class FavoritesController < ApplicationController
+    def index
+        @favorites = Favorite.all
+    end
+
+    def show
+        @favorite = Favorite.find(params[:id])
+    end
+
+    def new
+        @favorite = Favorite.new
+    end
+
+    def create
+        @favorite = Favorite.create(favorite_params)
+    end
+
+
+    def destroy
+        #im not sure with this one yet.
+    end
+
+    private
+
+    def favorite_params  
+        params.require(:favorite).permit(:room_id, :user_id)
+    end
+
+
 end
--- EmojiJabber/app/controllers/messages_controller.rb
+++ EmojiJabber/app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+    
+
+    def new
+        @messages = Message.new
+    end
+
+    def create
+        @message = message.create(favorite_params)
+    end
+
+
+    
+
+    private
+
+    def favorite_params  
+        params.require(:message).permit(:content, :room_id, :user_id)
+    end
+
 end
